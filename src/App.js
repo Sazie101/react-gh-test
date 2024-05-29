c4d31b22a1aa129dd1625418bcbd489aac11b885
Samuel Yazie <samuelwassie169@gmail.com>
2024-05-29 14:28:49 -0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// App is the main component
+/**
+ * Browsers can't read 
+ * 
+ * Conventios
+ *  - All elements must be closed 
+ *  - Empty elements (br, hr, img, input...) must be closed with a slash(/)
+ *  - JSX uses camelCase style onclick
+ *  - Use curly braces to include literal Javascript
+ *  - Use double curly braces with objects
+ * 
+ */
+
+// Spoiler: this is a hook
+import { useState } from "react";
 
 function App() {
+  const [userName, setUserName] = useState('');
+
+  const handleInput = event => {
+    setUserName(event.target.value);
+  }
+
+  let message = <h2>Welcome to React, <span>{userName}</span></h2>
+
+  let title = 'Intro to React';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        {message}
+        <input 
+          type="text"
+          className="input"
+          placeholder="User name"
+          onChange={handleInput}
+        />
+      </div>
+    </main>
   );
 }
 
